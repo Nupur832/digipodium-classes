bfb4a8c9ac9b9626b542437eec4537474f22ca28
Nupur832 <111993959+Nupur832@users.noreply.github.com>
2022-09-02 18:56:42 +0530

    datatypes.py,op1.py,op2.py,op3.py,op4.py

---
+++ datatypes.py
@@ -0,0 +1,58 @@
+x = 51230
+y = -2478574
+z = 24689531568057
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+x = 1.20
+y = 32.543
+z = .431
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+name = "bruce wayne 01"
+color = 'yellow'
+Institute = 'digipodium'
+print(name, type(name))
+print(color, type(color))
+print(Institute, type(Institute))
+
+is_coding = True
+is_fun = True
+are_u_bored = False
+print(is_coding, type(is_coding))
+print(is_fun, type(is_fun))
+print(are_u_bored, type(are_u_bored))
+
+x = None
+print(x, type(x))
+
+a = [5,10,15,20,25,30,35,40]
+vals = [1,2.2,'python']
+colors = ['red','yellow','green']
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+a = (5,10,15,20,25,30,35,40)
+vals = (1,2.2,'python')
+colors = ('red','yellow','green')
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+a = {5,10,15,20,25,30,35,40}
+vals = {1,2.2,'python'}
+colors = {'red','yellow','green'}
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+car_info = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year"  : 1964
+}
+print(car_info, type(car_info))
---
+++ op2.py
@@ -0,0 +1,19 @@
+print('ASSIGNMENT OPERATORS')
+
+x = 15 # simple assignment
+y = x + 15
+z = x + y
+a = x**2 + y**2
+
+print(x, y, z, a)
+
+b = 20
+b += 5 # updation assignment
+print(b)
+b -= 2 # updation assignment
+print(b)
+b *= 3
+print(b)
+b /= 2
+print(b)
+
