1579fed8d3f9bc4680bd5f4678fe21067e408080
Nupur832 <111993959+Nupur832@users.noreply.github.com>
2022-09-02 21:51:59 +0530

    area.py

---
+++ display.py
@@ -0,0 +1,10 @@
+name = 'william'
+age = 40
+
+# My name is Raj and i am 19 years old
+# comma seperation
+print('My name is', name, 'and i am', age, 'years old')
+print('My name is', name, 'and i am', age, 'years old', sep='')
+
+# f-string (important)
+print(f'my name is {name} \n\n and i am {age} years old')
