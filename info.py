d5e89db049c558beeec05519e31eb63c874c023a
Nupur832 <111993959+Nupur832@users.noreply.github.com>
2022-09-02 19:34:22 +0530

    op6.py

---
+++ info.py
@@ -0,0 +1,7 @@
+name = input("Enter ur name: ")
+college = input("Where do you study: ")
+city = input('City name: ')
+age = input("Enter ur age: ")
+animal = input("National animal of India: ")
+
+print(name, college, city, age, animal)
